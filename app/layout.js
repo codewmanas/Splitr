1f73c283aba5201870d7931c2c2464a7a7edcb22
codewmanas <manas.kolaskar@somaiya.edu>
2025-05-17 17:35:51 +0530

    Convex Initialized

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,15 +16,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body
         className={`${inter.className}`}
       >
+        <ConvexClientProvider>
         <Header />
       <main className="min-h-screen">
         {children}
       </main>
+      </ConvexClientProvider>
       </body>
     </html>
   );
